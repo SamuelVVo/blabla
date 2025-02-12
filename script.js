59ed5c45115bd2672876db7e35e6227fa9d59dd8
SamuelVVo <116473504+SamuelVVo@users.noreply.github.com>
2025-02-11 22:47:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 function toggleBackground() {
     const body = document.body;
-    if (body.style.backgroundColor === 'white') {
-        body.style.backgroundColor = 'black';
-        body.style.color = 'white';
-    } else {
-        body.style.backgroundColor = 'white';
-        body.style.color = 'black';
-    }
+    body.classList.toggle('dark-mode');
 }
